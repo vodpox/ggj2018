a5c67e742c76774d331ad9fa5544965ce7c1c9b3
vodpox <edvinas.senda@gmail.com>
2018-01-27 02:55:26 +0200

    Added player + main scene

---
+++ Scenes/Player.gd
@@ -0,0 +1,80 @@
+extends Area2D
+
+
+export (int) var speed
+export (int) var maxHealth
+export (PackedScene) var soundWave
+export (int) var shootVolume
+export (int) var hitVolume
+var health
+var isReloaded = true
+var velocity = Vector2()
+
+
+func _ready():
+	#call_deferred("set_monitoring", false)
+	#hide()
+	pass
+
+
+func _process(delta):
+	
+	if Input.is_action_pressed("shoot"):
+		if isReloaded:
+			shoot()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
+	
+	position += velocity * speed
+	rotation = position.angle_to_point(get_viewport().get_mouse_position()) + PI
+	
+	velocity.x = 0
+	velocity.y = 0
+
+
+func shoot():
+	
+	var shootWave = soundWave.instance();
+	add_child(shootWave);
+	shootWave.start($BulletRay.position, shootVolume)
+	
+	if $BulletRay.is_colliding():
+		
+		var hitWave = soundWave.instance();
+		#add_child(hitWave);
+		hitWave.start($BulletRay.position.x, hitVolume)
+		
+	isReloaded = false
+	$ReloadTimer.start()
+
+
+func _on_ReloadTimer_timeout():
+	isReloaded = true
+
+
+func spawn(pos):
+	position = pos
+	monitoring = true
+	health = maxHealth
+	show()
+	monitoring = true
+
+
+
+func die():
+	call_deferred("set_monitoring", false)
+	hide()
